1f7e15d3956f98754f909029b1e2c547cbf1c7c1
Acexy <thankjava@gmail.com>
2023-08-02 14:21:34 +0800

    测试验证统一模块加载的接口声明

--- parentmodule/declaration/module.go
+++ parentmodule/declaration/module.go
@@ -2,11 +2,25 @@ package declaration
 
 import (
 	"errors"
+	"sort"
 	"strconv"
+	"sync"
 )
 
 type Module struct {
-	moduleLoaders []ModuleLoader
+	ModuleLoaders []ModuleLoader
+}
+
+const (
+	defaultConfigUnregisterPriority       = uint(9999)
+	defaultConfigUnregisterMaxWaitSeconds = 20
+)
+
+// ShutdownResult 模块停止卸载结果
+type ShutdownResult struct {
+	ModuleName string
+	Err        error
+	Gracefully bool
 }
 
 // ModuleConfig 卸载模块时对应的配置
@@ -22,10 +36,24 @@ type ModuleConfig struct {
 
 	// 是否允许该模块异步卸载
 	// true	执行异步卸载，触发卸载后不立即等待卸载结果
-	UnregisterAllowSync bool
+	UnregisterAllowAsync bool
 
 	// 等待优雅停机的最大时间 (秒)
 	UnregisterMaxWaitSeconds uint
+}
+
+type sortedModuleByUnregisterPriority []ModuleLoader
+
+func (c sortedModuleByUnregisterPriority) Len() int {
+	return len(c)
+}
+
+func (c sortedModuleByUnregisterPriority) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c sortedModuleByUnregisterPriority) Less(i, j int) bool {
+	return c[i].ModuleConfig().UnregisterPriority < c[j].ModuleConfig().UnregisterPriority
 }
 
 // ModuleLoader 声明starter加载的通用接口
@@ -47,18 +75,17 @@ type ModuleLoader interface {
 	Unregister(maxWaitSeconds uint) (gracefully bool, err error)
 }
 
-// Load 依次启动每个模块
-// 采用同步的模式，仅当上一个模块启动正常后执行后续启动
-func (m *Module) Load(loaders []ModuleLoader) error {
-	if len(loaders) != 0 {
+// Load 依次加载启动每个模块
+// 采用同步的模式，仅当上一个模块启动正常后执行后续启动，任何模块的错误将中断并返回异常
+func (m *Module) Load() error {
+	if len(m.ModuleLoaders) != 0 {
 		var err error
-		for _, loader := range loaders {
+		for _, loader := range m.ModuleLoaders {
 			err = loader.Register(loader.Interceptor())
 			if err != nil {
 				return err
 			}
 		}
-		m.moduleLoaders = loaders
 		return nil
 	} else {
 		return errors.New("nil module loader")
@@ -67,32 +94,83 @@ func (m *Module) Load(loaders []ModuleLoader) error {
 
 // Unload 依次卸载每个模块 仅在上一个模块成功卸载后处理下一个 忽略UnregisterConfig配置
 // param	maxWaitSeconds 等待优雅停机的最大时间(秒) 该时间将分别作用于每个模块
-// return	返回每个模块名对应是否优雅停机结果
-func (m *Module) Unload(maxWaitSeconds uint) (map[string]bool, error) {
-	shutdownResult := make(map[string]bool, len(m.moduleLoaders))
-	for index, loader := range m.moduleLoaders {
+// return	map[string]ShutdownResult
+func (m *Module) Unload(maxWaitSeconds uint) []ShutdownResult {
+	shutdownResult := make([]ShutdownResult, len(m.ModuleLoaders))
+	for index, loader := range m.ModuleLoaders {
 		gracefully, err := loader.Unregister(maxWaitSeconds)
+		var moduleName string
+		if loader.ModuleConfig() == nil || loader.ModuleConfig().ModuleName == "" {
+			moduleName = "unnamed " + strconv.Itoa(index)
+		} else {
+			moduleName = loader.ModuleConfig().ModuleName
+		}
 		if err == nil {
-			if loader.ModuleConfig() == nil || loader.ModuleConfig().ModuleName == "" {
-				shutdownResult["unnamed "+strconv.Itoa(index)] = gracefully
-			} else {
-				shutdownResult[loader.ModuleConfig().ModuleName] = gracefully
+			shutdownResult[index] = ShutdownResult{
+				ModuleName: moduleName,
+				Gracefully: gracefully,
 			}
 		} else {
-			return shutdownResult, err
+			shutdownResult[index] = ShutdownResult{
+				ModuleName: moduleName,
+				Err:        err,
+			}
 		}
 	}
-	return shutdownResult, nil
+	return shutdownResult
 }
 
 // UnloadByConfig 根据配置规则卸载模块，如果未配置config，将自动使用默认配置进行卸载
-// 默认配置 优先级最低(且不保证顺序) 同步卸载 最大优雅停机等待时机10s
-func (m *Module) UnloadByConfig(maxWaitSeconds uint) (map[string]bool, error) {
-	for _, loader := range m.moduleLoaders {
+// 默认配置： 优先级最低(且不保证顺序) 同步卸载 最大优雅停机等待时机20s
+func (m *Module) UnloadByConfig() []ShutdownResult {
+	allModuleConfig := make([]*ModuleConfig, len(m.ModuleLoaders))
+	for index, loader := range m.ModuleLoaders {
 		config := loader.ModuleConfig()
 		if config == nil {
+			config = &ModuleConfig{
+				ModuleName:               "unnamed " + strconv.Itoa(index),
+				UnregisterPriority:       defaultConfigUnregisterPriority,
+				UnregisterAllowAsync:     false,
+				UnregisterMaxWaitSeconds: defaultConfigUnregisterMaxWaitSeconds,
+			}
+		} else {
+			// 检查配置内容
+			if config.ModuleName == "" {
+				config.ModuleName = "unnamed " + strconv.Itoa(index)
+			}
+			if config.UnregisterMaxWaitSeconds == 0 {
+				config.UnregisterMaxWaitSeconds = defaultConfigUnregisterMaxWaitSeconds
+			}
+		}
+		allModuleConfig = append(allModuleConfig, config)
+	}
 
+	var wait sync.WaitGroup
+	wait.Add(len(m.ModuleLoaders))
+
+	sort.Sort(sortedModuleByUnregisterPriority(m.ModuleLoaders))
+
+	shutdownResult := make([]ShutdownResult, len(m.ModuleLoaders))
+
+	for index, loader := range m.ModuleLoaders {
+		shutdownResult[index] = ShutdownResult{ModuleName: loader.ModuleConfig().ModuleName}
+		if loader.ModuleConfig().UnregisterAllowAsync {
+			go unload(&wait, loader, &shutdownResult[index])
+		} else {
+			unload(&wait, loader, &shutdownResult[index])
 		}
 	}
-	return nil, nil
+
+	wait.Wait()
+	return shutdownResult
+}
+
+func unload(wait *sync.WaitGroup, loader ModuleLoader, shutdownResult *ShutdownResult) {
+	defer wait.Done()
+	gracefully, err := loader.Unregister(loader.ModuleConfig().UnregisterMaxWaitSeconds)
+	if err == nil {
+		shutdownResult.Gracefully = gracefully
+	} else {
+		shutdownResult.Err = err
+	}
 }
---
+++ parentmodule/declaration/module_test.go
@@ -0,0 +1,99 @@
+package declaration
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+	"time"
+)
+
+type Module1 struct {
+}
+
+func (Module1) ModuleConfig() *ModuleConfig {
+	return &ModuleConfig{ModuleName: "Module1", UnregisterPriority: 1, UnregisterAllowAsync: true}
+}
+
+func (Module1) Register(interceptor *func(instance interface{})) error {
+	return nil
+}
+
+func (Module1) Interceptor() *func(instance interface{}) {
+	return nil
+}
+
+func (Module1) Unregister(maxWaitSeconds uint) (gracefully bool, err error) {
+	time.Sleep(time.Second * 3)
+	return false, nil
+}
+
+type Module2 struct {
+}
+
+func (Module2) ModuleConfig() *ModuleConfig {
+	return &ModuleConfig{ModuleName: "Module2", UnregisterPriority: 2}
+}
+
+func (Module2) Register(interceptor *func(instance interface{})) error {
+	return nil
+}
+
+func (Module2) Interceptor() *func(instance interface{}) {
+	return nil
+}
+
+func (Module2) Unregister(maxWaitSeconds uint) (gracefully bool, err error) {
+	time.Sleep(time.Second * 1)
+	return true, nil
+}
+
+type Module3 struct {
+}
+
+func (Module3) ModuleConfig() *ModuleConfig {
+	return &ModuleConfig{ModuleName: "Module3", UnregisterPriority: 5}
+}
+
+func (Module3) Register(interceptor *func(instance interface{})) error {
+	return nil
+}
+
+func (Module3) Interceptor() *func(instance interface{}) {
+	return nil
+}
+
+func (Module3) Unregister(maxWaitSeconds uint) (gracefully bool, err error) {
+	time.Sleep(time.Second * 1)
+	return false, nil
+}
+
+func TestSortModuleByUnregisterPriority(t *testing.T) {
+	modules := []ModuleLoader{Module1{}, Module3{}, Module2{}}
+
+	for _, m := range modules {
+		fmt.Println(m.ModuleConfig().ModuleName, m.ModuleConfig().UnregisterPriority)
+	}
+	sort.Sort(sortedModuleByUnregisterPriority(modules))
+	fmt.Println("sorted")
+	for _, m := range modules {
+		fmt.Println(m.ModuleConfig().ModuleName, m.ModuleConfig().UnregisterPriority)
+	}
+}
+
+func TestLoadAndUnload(t *testing.T) {
+	m := Module{ModuleLoaders: []ModuleLoader{Module1{}, Module3{}, Module2{}}}
+	err := m.Load()
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		return
+	}
+
+	st := time.Now().UnixMilli()
+	result := m.Unload(10)
+	fmt.Printf("%+v\n cost %+d \n", result, time.Now().UnixMilli()-st)
+
+	st = time.Now().UnixMilli()
+	result = m.UnloadByConfig()
+	fmt.Printf("%+v\n cost %+d \n", result, time.Now().UnixMilli()-st)
+
+}
