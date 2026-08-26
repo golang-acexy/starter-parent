5725bea4d65e249118f336ebd298c85e24c825ac
Acexy <thankjava@gmail.com>
2026-08-26 15:35:30 +0800

    Improve starter lifecycle concurrency and validation

--- parent/error.go
+++ parent/error.go
@@ -3,12 +3,16 @@ package parent
 import "errors"
 
 var (
-	ErrMissStarters           = errors.New("miss starters")
 	ErrNoStarter              = errors.New("no starter")
-	ErrNoStarterSet           = errors.New("no starter set")
+	ErrMissStarters           = ErrNoStarter
+	ErrNoStarterSet           = ErrNoStarter
 	ErrUnknownStarterName     = errors.New("unknown starterName")
 	ErrSomeStarterNoSetting   = errors.New("some starter has no setting")
+	ErrNilStarter             = errors.New("starter is nil")
+	ErrEmptyStarterName       = errors.New("starter name is empty")
+	ErrDuplicateStarterName   = errors.New("duplicate starter name")
 	ErrStopAllTimeout         = errors.New("stop the module exceeding the maximum wait time")
 	ErrStarterNotStarted      = errors.New("not started")
+	ErrStarterStopping        = errors.New("starter is stopping")
 	ErrStarterRestartDisabled = errors.New("starter restart disabled")
 )
